d05acb3c8c27d06fd5183c5c3bd441ca535e379a
Priyanshu Seth <55964282+aham-brahmasmi@users.noreply.github.com>
2024-03-31 14:30:58 +0530

    Update p_0.sql
    update in question 8

--- practice_set/p_0.sql
+++ practice_set/p_0.sql
@@ -78,7 +78,7 @@ select c.customer_id,concat(c.first_name," ",c.last_name) full_name,o.order_id,o
 left join orders o on c.customer_id=o.customer_id;
 
 -- 8.Write a Query to customer name in order of creditLimit smallest to highest.
-select concat(first_name," ",last_name) full_name from customers order by creditLimit asc;
+select concat(first_name," ",last_name) full_name,creditLimit from customers order by creditLimit asc;
 select * from customers;
 
 -- 9.  Write a stored procedure by name order_day. The procedure should show the customer_id and the
